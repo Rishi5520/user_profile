72ffbb763a9fddfbd267fbc2d6f6300e189da6df
Rishiraj shinde <rishirajshinde1610@gmail.com>
2024-03-31 19:53:24 +0530

    UI_Emporve_and Change

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,8 @@
 module.exports = {
   content: [
     './src/**/*.{js,jsx,ts,tsx}',
-    'node_modules/flowbite-react/lib/esm/**/*.js'
+    'node_modules/flowbite-react/lib/esm/**/*.js',
+    "./node_modules/flowbite/**/*.js"
   ],
   theme: {
     extend: {
